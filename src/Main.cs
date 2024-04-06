342ad402a3f563fc595082cc39958abdd1048ea1
Vincent Sit <vincentsit00@ymail.com>
2024-04-07 10:01:25 +1200

    Fix button onpress function bug

--- src/Main.cs
+++ src/Main.cs
@@ -1,2 +1,16 @@
 using Godot;
 
+public class Main : Node2D
+{
+    public override void _Ready()
+    {
+        var startButton = (MenuButton) GetNode("Start");
+        startButton.OnPress = () => { GD.Print("Hello - start"); };
+
+        var settingButton = (MenuButton) GetNode("Settings");
+        settingButton.OnPress = () => { GD.Print("Hello - settings"); };
+
+        var quitButton = (MenuButton) GetNode("Exit");
+        quitButton.OnPress = () => { GetTree().Quit(); };
+    }
+}
--- src/main-title/MenuButton.cs
+++ src/main-title/MenuButton.cs
@@ -6,25 +6,20 @@ public class MenuButton : MarginContainer
     [Export]
     public string InnerString { get; set; } = "Ha ha";
     public Label InnerLabel { get; set; }
+    public Button InnerButton { get; set; }
     public Action OnPress { get; set; }
-
-    // public MenuButton(string innerText, Action onPress)
-    // {
-    //     InnerString = innerText;
-    //     OnPress = onPress;
-    // }
+    
 
     public override void _Ready()
     {
         InnerLabel = (Label) GetNode("Label");
         InnerLabel.Text = InnerString;
-        var button = (Button)GetNode("Button");
-        button.Connect("pressed", this, nameof(OnPressHandler));
+        InnerButton = (Button) GetNode("Button");
+        InnerButton.Connect("pressed", this, nameof(OnPressHandler));
     }
 
-    public void OnPressHandler()
+    private void OnPressHandler()
     {
-        //OnPress();
-        GD.Print("Hello!");
+        OnPress();
     }
 }
